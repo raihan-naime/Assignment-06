06e731ae45009ec8539564b555e8d8ab2aa0123e
RaihanNaim <imraihan.naim@gmail.com>
2025-09-09 21:32:20 +0600

    update code

--- scripts/add-to-cart.js
+++ scripts/add-to-cart.js
@@ -1,31 +1,34 @@
-const billCard = [];
-const loadAddToCardBtns = () => {
-  const url = "https://openapi.programming-hero.com/api/plants";
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) => AddToCard(data.plants));
-};
+// const billCard = [];
+// const loadAddToCardBtns = () => {
+//   const url = "https://openapi.programming-hero.com/api/plants";
+//   fetch(url)
+//     .then((res) => res.json())
+//     .then((data) => AddToCard(data.plants));
+// };
 
-const AddToCard = (plants) => {
-    const addToCartContainer = document.getElementById('add-to-card-container');
-  plants.forEach((plant) => {
-    const div = document.createElement('div');
-    div.innerHTML = `
-    <div class="flex justify-between items-center bg-[#f0fdf4] m-2 rounded-lg p-5">
-                <div>
-                  <h3 class="text-2xl font-medium">${plant.Name}</h3>
-                <h3 class="text-xl"> <i class="fa-solid fa-bangladeshi-taka-sign"></i> ${plant.price}</h3>
-                </div>
-                <div onclick="removeCard()" class="h-10 w-10 flex justify-center items-center order-cancel ">
-                  ❌
-                </div>
-              </div>
-    `;
-    // addToCartContainer.appendChild(div);
+// const AddToCard = (plants) => {
+//     const addToCartContainer = document.getElementById('add-to-card-container');
+//   plants.forEach((plant) => {
+//     const div = document.createElement('div');
+//     div.innerHTML = `
+//     <div class="flex justify-between items-center bg-[#f0fdf4] m-2 rounded-lg p-5">
+//                 <div>
+//                   <h3 class="text-2xl font-medium">${plant.Name}</h3>
+//                 <h3 class="text-xl"> <i class="fa-solid fa-bangladeshi-taka-sign"></i> ${plant.price}</h3>
+//                 </div>
+//                 <div onclick="removeCard()" class="h-10 w-10 flex justify-center items-center order-cancel ">
+//                   ❌
+//                 </div>
+//               </div>
+//     `;
+//     // addToCartContainer.appendChild(div);
     
-  });
-//   billCard.push(billCard);
-};
+//   });
+// //   billCard.push(billCard);
+// };
+
+
+// =============================================================
 // console.log(billCard);
 
 // {
@@ -36,3 +39,5 @@ const AddToCard = (plants) => {
 //     "category": "Aquatic Plant",
 //     "price": 250
 // }
+
+// console.log();
