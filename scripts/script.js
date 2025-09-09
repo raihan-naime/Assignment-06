195dc9379de5dc823da8063db09c864dc15986fe
RaihanNaim <imraihan.naim@gmail.com>
2025-09-09 23:07:31 +0600

    update

--- scripts/script.js
+++ scripts/script.js
@@ -129,7 +129,7 @@ const showAllPlants = (plants) => {
                     </div>
                   </div>
                   <div class="card-actions w-full">
-                    <button onclick="loadAddToCardBtns()" class="btn hover:bg-green-800 rounded-lg bg-[#15803d] w-full text-white add-to-card">Add to Cart</button>
+                    <button onclick="loadAddToCardBtns(${ plant.id}, ${plant.price})" class="btn hover:bg-green-800 rounded-lg bg-[#15803d] w-full text-white add-to-card">Add to Cart</button>
                   </div>
                 </div>
               </div>
@@ -206,3 +206,32 @@ const manageSpinner = (status) => {
 }
 
 // ❗❗❗❗❗❗remove bill❗❗❗❗❗❗
+const billCard = [];
+const loadAddToCardBtns = (plantId, plantPrice) => {
+  console.log(plantId, plantPrice);
+  const url = "https://openapi.programming-hero.com/api/plants";
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => AddToCard(data.plants));
+};
+
+const AddToCard = (plants) => {
+    const addToCartContainer = document.getElementById('add-to-card-container');
+  plants.forEach((plant) => {
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <div class="flex justify-between items-center bg-[#f0fdf4] m-2 rounded-lg p-5">
+                <div>
+                  <h3 class="text-2xl font-medium">${plant.Name}</h3>
+                <h3 class="text-xl"> <i class="fa-solid fa-bangladeshi-taka-sign"></i> ${plant.price}</h3>
+                </div>
+                <div onclick="removeCard()" class="h-10 w-10 flex justify-center items-center order-cancel ">
+                  ❌
+                </div>
+              </div>
+    `;
+    // addToCartContainer.appendChild(div);
+    
+  });
+//   billCard.push(billCard);
+};
