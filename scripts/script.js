cccb2fe378c07f4b6fa58a2bde206110dfd24a49
RaihanNaim <imraihan.naim@gmail.com>
2025-09-09 10:49:30 +0600

    added spinner

--- scripts/script.js
+++ scripts/script.js
@@ -2,12 +2,10 @@
 const removeActive = () => {
   const categoryBtns = document.querySelectorAll(".category-btn");
   // console.log(categoryBtns);
-  categoryBtns.forEach(btn => {
-    btn.classList.remove('active');
+  categoryBtns.forEach((btn) => {
+    btn.classList.remove("active");
   });
 };
-
-
 
 // ❗❗❗❗❗❗Categories section❗❗❗❗❗❗
 
@@ -21,6 +19,7 @@ const getCategories = () => {
 // display categories
 const displayCategories = (categories) => {
   const categoryContainer = document.getElementById("category-container");
+  categoryContainer.innerHTML = "";
   categories.forEach((category) => {
     // console.log(category.id)
     const div = document.createElement("div");
@@ -36,6 +35,7 @@ getCategories();
 
 // ❗❗❗❗❗❗main section❗❗❗❗❗❗
 const loadCategoryBasedPlants = (id) => {
+  manageSpinner(true);
   const url = `https://openapi.programming-hero.com/api/category/${id}`;
   fetch(url)
     .then((res) => res.json())
@@ -79,13 +79,14 @@ const displayCategoryBasedPlants = (categoryPlants) => {
                     </div>
                   </div>
                   <div class="card-actions w-full">
-                    <button class="btn hover:bg-green-800 rounded-lg bg-[#15803d] w-full text-white">Add to Cart</button>
+                    <button class="btn hover:bg-green-800 rounded-lg bg-[#15803d] w-full text-white add-to-card">Add to Cart</button>
                   </div>
                 </div>
               </div>
     `;
     cardContainer.appendChild(div);
   });
+  manageSpinner(false);
 };
 
 // ❗❗❗❗❗❗show all plants by default❗❗❗❗❗❗
@@ -95,8 +96,8 @@ const loadAllPlants = () => {
     .then((res) => res.json())
     .then((data) => showAllPlants(data.plants));
 };
-
 const showAllPlants = (plants) => {
+    
   const cardContainer = document.getElementById("card-container");
   cardContainer.innerHTML = "";
   plants.forEach((plant) => {
@@ -126,7 +127,7 @@ const showAllPlants = (plants) => {
                     </div>
                   </div>
                   <div class="card-actions w-full">
-                    <button class="btn hover:bg-green-800 rounded-lg bg-[#15803d] w-full text-white">Add to Cart</button>
+                    <button class="btn hover:bg-green-800 rounded-lg bg-[#15803d] w-full text-white add-to-card">Add to Cart</button>
                   </div>
                 </div>
               </div>
@@ -137,23 +138,62 @@ const showAllPlants = (plants) => {
 loadAllPlants();
 
 // ❗❗❗❗❗❗modal❗❗❗❗❗❗
-const loadPlantsDetails = (id) =>{
+const loadPlantsDetails = (id) => {
   const url = `https://openapi.programming-hero.com/api/plant/${id}`;
   fetch(url)
-  .then(res => res.json())
-  .then(data => displayPlantDetails(data.plants))
-}
+    .then((res) => res.json())
+    .then((data) => displayPlantDetails(data.plants));
+};
 const displayPlantDetails = (plantsInfo) => {
-  console.log(plantsInfo);
-  const modalContainer = document.getElementById('modal-container');
+  // console.log(plantsInfo);
+  const modalContainer = document.getElementById("modal-container");
   modalContainer.innerHTML = `
   <h3 class="text-3xl font-bold">${plantsInfo.name}!</h3>
-          <img  src="${plantsInfo.image}" alt="" />
+          <img src="${plantsInfo.image}" alt="" />
           <p class="font-bold text-xl">
             Category : <span class="text-gray-500"> ${plantsInfo.category}</span>
           </p>
           <p class="font-bold text-xl">Price : ${plantsInfo.price}</p>
           <p class="py-4"><span class="font-bold">Description</span>: ${plantsInfo.description}</p>
-  `
-  document.getElementById('my_modal_5').showModal();
+  `;
+  document.getElementById("my_modal_5").showModal();
+};
+
+// ❗❗❗❗❗❗Challenge Part❗❗❗❗❗❗
+const cardContainer = document.getElementById('card-container')
+cardContainer.addEventListener('click', (e) =>{
+  const btn = e.target.closest('.add-to-card');
+  if(btn){
+    const addToCartContainer = document.getElementById('add-to-card-container');
+    const price = Number(btn.parentNode.parentNode.children[2].children[1].children[0].children[1].innerText);
+    const plantName = btn.parentNode.parentNode.children[0].innerText;
+    alert(`${plantName} has been added in the card`);
+    
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <div class="flex justify-between items-center bg-[#f0fdf4] m-2 rounded-lg p-5">
+                <div>
+                  <h3 class="text-xl font-medium">${plantName}</h3>
+                <h3 class="text-xl">${price}</h3>
+                </div>
+                <div class="h-10 w-10 flex justify-center items-center order-cancel ">
+                  ❌
+                </div>
+              </div>
+    `;
+    addToCartContainer.appendChild(div);
+  }
+
+})
+
+
+// ❗❗❗❗❗❗manage spinner❗❗❗❗❗❗
+const manageSpinner = (status) => {
+  if(status){
+    document.getElementById('spinner').classList.remove('hidden');
+    document.getElementById('card-container').classList.add('hidden');
+  }else{
+    document.getElementById('spinner').classList.add('hidden');
+    document.getElementById('card-container').classList.remove('hidden');
+  }
 }
